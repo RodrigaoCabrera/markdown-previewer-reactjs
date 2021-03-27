4bf1e57c0fbe159722f933ee9d4ee2b2e87ebafb
rodrigocabrera19 <rodrigod33d@gmail.com>
2021-03-27 14:29:21 -0300

    add editor and previewer

--- src/App.js
+++ src/App.js
@@ -5,43 +5,35 @@ import './index.css'
 
 const App = () => {
    const [data, setData] = useState('');
-
+   
    const onChange = (e) => {
     setData(e.target.value)
    }
-   
+
+const onKeyPressHandler = (event) => {
+    if (event.keyCode === 13) {
+        
+    }
+  }
+  
 return(
-<div id="editor" style={{
-    display:'flex',
-    flexDirection: 'column',
-    alignItems:'center'
-}}>
-    <div style={{
-            width: '60%'
-        }}>
-        <h3 style={{
-            backgroundColor: 'grey'
-        }}>Editor</h3>
-        <textarea value={data} onChange={onChange} style={{
+    
+<div id="editor">
+    
+    <div>
+        <h3>Editor</h3>
+        <textarea value={data} onChange={onChange} onKeyDown={onKeyPressHandler} style={{
             width: '100%'
         }}></textarea>
         
     </div>
 
-    <div style={{
-            backgroundColor:'grey',
-            width: '80%'
-        }}>
-            <h3 style={{
-            backgroundColor: 'blue'
-        }}>Editor</h3>
-        <p style={{
-            border: '2px solid grey'
-        }}>{data}</p>
+    <div>
+            <h3>Previewer</h3>
+            <div>{data}</div>
+        
     </div>
-    <div style={{
-            backgroundColor: 'grey'
-        }}></div>
+    
 </div>
 
 )};
